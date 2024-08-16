b5b01804c7b8cccd01af2760fbe1277b18ff8e95
silent-killer132 <143581664+silent-killer132@users.noreply.github.com>
2024-08-16 14:11:10 +0530

    New Home page added

---
+++ MainFrontend/Frontend/src/assets/index.js
@@ -0,0 +1,41 @@
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import loading from "./loading.png";
+import background from "./background.jpg";
+import curve from "./hero/curve.png";
+import heroBackground from "./hero/hero-background.jpg";
+import notification1 from "./notification/image-1.png";
+import notification2 from "./notification/image-2.png";
+import notification3 from "./notification/image-3.png";
+import notification4 from "./notification/image-4.png";
+import benefitImage2 from "./benefits/image-2.png";
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+ 
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  
+  loading,
+  background,
+  curve,
+    heroBackground,
+      notification1,
+  notification2,
+  notification3,
+  notification4,
+  benefitImage2,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
