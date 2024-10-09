92871289fb69f6936021f4312d02ffb053824a7c
silent-killer132 <143581664+silent-killer132@users.noreply.github.com>
2024-10-10 00:16:37 +0530

    ApiError replaced with ApiResponse

--- backend/src/controllers/verifyEmail.controller.js
+++ backend/src/controllers/verifyEmail.controller.js
@@ -1,27 +1,35 @@
 import { User } from "../models/userSchema.js";
-import { ApiError } from "../utils/ApiError.js";
+// import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 export async function verifyEmail(req,res){
 try {
         const {id} = req.params;
         const {otp} = req.body;
-        if(!otp) return res.json(new ApiError(404, 'Otp is required to verify'))
+        if(!otp) 
+            // return res.json(new ApiError(404, 'Otp is required to verify'))
+        return res.status(404).json(new ApiResponse('Otp is required to verify',{success: false}))
         const client = await User.findOne(
             {
                 username: id
             }
         )
-        if(!client) return res.json(new ApiError(404,'Invalid Username'));
+        if(!client) 
+            // return res.json(new ApiError(404,'Invalid Username'));
+        return res.status(404).json(new ApiResponse('Invalid Username',{success: false}))
         const currDate = new Date(Date.now())
         const isOtpValid = client.otpExpiry > currDate;
-        if(!isOtpValid) return res.status(410).json(new ApiError(410, 'Otp is Expired'))
+        if(!isOtpValid) 
+            // return res.status(410).json(new ApiError(410, 'Otp is Expired'))
+        return res.status(410).json(new ApiResponse('Otp is Expired',{success: false}))
         const isOtpCorrect = client.otp ===otp;
-        if(!isOtpCorrect) return res.status(410).json(new ApiError(401, 'Invalid Otp'))
+        // if(!isOtpCorrect) return res.status(401).json(new ApiError(401, 'Invalid Otp'))
+        if(!isOtpCorrect) return res.status(401).json(new ApiResponse('Invalid Otp',{success: false}))
         client.isEmailVerified = true;
         await client.save();
         return res.status(200).json(new ApiResponse('Verification Successful',{success: true}))
 } catch (error) {
-    return res.json(new ApiError(500,'Server is Down!'))
+    // return res.json(new ApiError(500,'Server is Down!'))
+    return res.status(500).json(new ApiResponse('Server is Down!',{success: false}))
 }
 }
