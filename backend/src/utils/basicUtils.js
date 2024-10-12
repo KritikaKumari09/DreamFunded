6e112df5dbb1e7929c2c0ce66fe090cd05b1c1eb
Hrishabh Patel <hrishabhpatel2023@gmail.com>
2024-10-12 22:26:27 +0530

    Refactor package.json files to add otp-generator dependency

--- backend/src/utils/basicUtils.js
+++ backend/src/utils/basicUtils.js
@@ -1,9 +1,18 @@
+import otpGenerator from "otp-generator"
+
+// ! This method should not be used for otp-genration as Math.Random() outputs are predictable from past outputs
+// export const generateOtp = () =>{
+//     const digits = "0123456789"
+//     let otp = "";
+//     for(let i=0;i<6;i++){
+//         const idx = Math.floor(Math.random()*10)
+//         otp+=digits[idx];
+//     }
+//     return otp;
+// }
+
+//* this package uses crypto for generating otp
 export const generateOtp = () =>{
-    const digits = "0123456789"
-    let otp = "";
-    for(let i=0;i<6;i++){
-        const idx = Math.floor(Math.random()*10)
-        otp+=digits[idx];
-    }
-    return otp;
+    return otpGenerator.generate(6,{digits: true, lowerCaseAlphabets: false,specialChars: false, upperCaseAlphabets: false})
 }
+
