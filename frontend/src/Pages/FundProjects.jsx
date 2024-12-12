a4911a17999d3ef19b35a05464441811de657fd3
silent-killer132 <143581664+silent-killer132@users.noreply.github.com>
2024-12-12 21:28:31 +0530

    Fund projects page created

---
+++ frontend/src/Pages/FundProjects.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DOMPurify from 'dompurify';
+import Navbar from '../components/Navbar.jsx';
+import { curve } from '../assets/index.js';
+import { BackgroundBeams } from "../components/ui/background-beams.jsx"; // Assuming BackgroundBeams is a separate component.
+
+const FundProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/project/FundProjects', { withCredentials: true });
+        setProjects(response.data.data.projects);
+      } catch (error) {
+        console.error('Error fetching projects:', error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <BackgroundBeams className="absolute inset-0" />
+      <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-10 lg:mb-[6.25rem]">
+      <BackgroundBeams className="absolute inset-0" />
+        <h1 className="h3 mt-6 mb-4">
+          <span className="inline-block relative">
+            Fund Projects{" "}
+            <img
+              src={curve}
+              className="absolute top-full left-0 w-full xl:-mt-2"
+              width={624}
+              height={28}
+              alt="curve"
+            />
+          </span>
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap justify-center">
+        {projects.map((project) => (
+          <div key={project._id} className="projcard m-4 p-4 ">
+            <p className="heading text-white font-bold text-lg mb-2">{project.name}</p>
+            <p className="heading text-white font-bold text-base mb-2">Project owner:</p>
+            <p className=" text-sm mb-2"> {project.owner?.username|| 'N/A'}</p>
+            <div
+              className="text-white text-sm mb-2"
+              style={{ maxHeight: '3em', overflow: 'hidden', textOverflow: 'ellipsis' }}
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  project.description.length > 100
+                    ? project.description.substring(0, 100) + '...'
+                    : project.description
+                ),
+              }}
+            />
+            <p className=" text-sm">Deadline: {new Date(project.deadline).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+           
+            <p className="text-white text-sm">Status: {project.status}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default FundProjects;
