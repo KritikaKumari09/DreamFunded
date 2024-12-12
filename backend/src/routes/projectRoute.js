05804f096ef459fcfef516b5a30cbbec8ce7a85c
silent-killer132 <143581664+silent-killer132@users.noreply.github.com>
2024-12-12 21:24:16 +0530

    route for fundProjects page added

--- backend/src/routes/projectRoute.js
+++ backend/src/routes/projectRoute.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import {upload} from '../middlewares/multer.middlewares.js'
 import addProject from "../controllers/project/addProject.controller.js";
 import getAllProjects from "../controllers/project/getAllProjects.js";
+import FundProjects from "../controllers/project/FundProjects.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.post("/addProject",upload.none(),addProject);
 router.get("/getAllProject",verifyJWT,getAllProjects)
+router.get("/FundProjects",FundProjects)
+
 export default router;
